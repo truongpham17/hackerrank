8a426eed5c08351fdfb93da6fa290a65f587a9b5
Truong Pham <truong.pham@zoi.tech>
2024-06-20 16:49:16 +0700

    daily commit

---
+++ src/patching-array/index.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/patching-array/?envType=daily-question&envId=2024-06-20
+// HARD
+/**
+ * Given a sorted integer array nums and an integer n, 
+ * add/patch elements to the array such that any number in the range [1, n] inclusive can be formed by the sum of some elements in the array.
+
+Return the minimum number of patches required.
+1 <= nums.length <= 1000
+1 <= nums[i] <= 10**4
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number}
+ */
+var minPatches = function (nums, n) {
+
+};
